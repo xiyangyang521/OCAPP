945bfdb6c9d9a01f799790f5765c887b2f5b3ddf
贾富阳 <471267673@qq.com>
2019-05-23 13:51:25 +0800

    上传项目OCAPP

---
+++ OCAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCAPP
+//
+//  Created by 贾富阳 on 2019/5/23.
+//  Copyright © 2019 JiaFuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCAPP
+//
+//  Created by 贾富阳 on 2019/5/23.
+//  Copyright © 2019 JiaFuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
